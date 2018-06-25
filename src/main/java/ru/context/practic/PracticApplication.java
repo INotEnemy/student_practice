2243ebcf86b444e10990ff9c622c8eef5924a3d6
INotEnemy <vladiok2@mail.ru>
2018-06-25 13:42:01 +0300

    Init

---
+++ src/main/java/ru/context/practic/PracticApplication.java
@@ -0,0 +1,12 @@
+package ru.context.practic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticApplication.class, args);
+	}
+}
