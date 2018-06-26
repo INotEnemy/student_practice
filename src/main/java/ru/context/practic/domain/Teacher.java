07140ae86b926e0fd6deb9bf70c57414aecdc7ba
INotEnemy <vladiok2@mail.ru>
2018-06-26 15:37:26 +0300

    added Teacher

---
+++ src/main/java/ru/context/practic/domain/Teacher.java
@@ -0,0 +1,88 @@
+package ru.context.practic.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String department;
+
+    @Column
+    private String post;
+
+    @Column
+    private double load;
+
+    private ArrayList<Timetable> worksChedule ;
+
+    public Teacher() {
+    }
+
+    public Teacher(Integer id, String firstName, String lastName, String department, String post, double load) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.post = post;
+        this.load = load;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public double getLoad() {
+        return load;
+    }
+
+    public void setLoad(double load) {
+        this.load = load;
+    }
+
+
+
+}
