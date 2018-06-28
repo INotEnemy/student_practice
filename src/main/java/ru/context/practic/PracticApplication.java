d693b0f742ca960dadd939cc6077e13dca432c2b
INotEnemy <33321705+INotEnemy@users.noreply.github.com>
2018-06-28 22:00:41 +0300

    PracticAplication_update: added some javaDoc

--- src/main/java/ru/context/practic/PracticApplication.java
+++ src/main/java/ru/context/practic/PracticApplication.java
@@ -3,7 +3,10 @@ package ru.context.practic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+/**
+@param exclude - принимает значение  SecurityAutoConfiguration.class
+	для отключения проверки аутентификации на этапе разработки и тестирования приложения
+*/
 @SpringBootApplication(exclude = {
 		SecurityAutoConfiguration.class
 })
