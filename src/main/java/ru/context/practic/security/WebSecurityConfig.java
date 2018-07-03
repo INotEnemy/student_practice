28f57d0298319fdb1e387b9a9ef478f764682776
INotEnemy <vladiok2@mail.ru>
2018-07-03 17:48:48 +0300

    added Security for Teacher and Student controllers

---
+++ src/main/java/ru/context/practic/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package ru.context.practic.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Profile("dot")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/student/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/teacher/*").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("deploy").password("{noop}1234").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234qwerty").roles("ADMIN", "USER");
+    }
+
+}
+
